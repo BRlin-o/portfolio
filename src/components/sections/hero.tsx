94ad0c5421e88956861e6068f20ceb617f944502
br-mbp <brend.main@gmail.com>
2026-01-29 21:49:45 +0800

    feat(hero): update hero section with Typewriter effect

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button"
 import { ArrowRight, FileText } from "lucide-react"
 import { LazyMotion, domAnimation, m } from "motion/react"
 import * as motion from "motion/react-m"
+import { Typewriter } from "@/components/ui/typewriter-text"
+
 
 export function Hero() {
     return (
@@ -32,7 +34,16 @@ export function Hero() {
                         transition={{ duration: 0.5, delay: 0.1 }}
                         className="text-4xl font-bold tracking-tight sm:text-6xl md:text-7xl lg:text-8xl"
                     >
-                        Hi, I&apos;m <span className="bg-gradient-to-r from-blue-600 via-indigo-500 to-purple-600 bg-clip-text text-transparent dark:from-blue-400 dark:via-indigo-300 dark:to-purple-400">BR Lin</span>
+                        Hi, I&apos;m <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-indigo-500 to-purple-600 dark:from-blue-400 dark:via-indigo-300 dark:to-purple-400">
+                            <Typewriter
+                                text={["Cheng-Han", "Steven", "BR"]}
+                                speed={100}
+                                deleteSpeed={100}
+                                waitTime={3000}
+                            // cursorChar={"_"}
+                            // cursorClassName="ml-0"
+                            />Lin
+                        </span>
                     </motion.h1>
 
                     <motion.p
