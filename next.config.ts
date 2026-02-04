34830ed11214f5212e7d6a77a4821bebd32eae1e
br-mbp <brend.main@gmail.com>
2026-02-04 19:06:34 +0800

    feat: config hybrid deployment for Cloudflare and Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+const isExport = process.env.IS_EXPORT === "true";
+
 const nextConfig: NextConfig = {
+  output: isExport ? "export" : undefined,
+  images: {
+    unoptimized: isExport,
+  },
   /* config options here */
 };
 
